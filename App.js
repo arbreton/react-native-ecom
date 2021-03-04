0465e11736ad27c022f16642a8d4853d20de799b
arbreton <andrebreton@live.com>
2021-03-04 11:00:58 -0800

    Added Flatlist and zara sample images with styles

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Image, FlatList, View, StatusBar, Dimensions, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const {width, height} = Dimensions.get('screen');
+
+const ITEM_WIDTH = width;
+const ITEM_HEIGHT = height * .75;
+
+const images = [
+    'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_1_1_1.jpg?ts=1606727905128',
+    'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_2_1_1.jpg?ts=1606727908993',
+    'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_2_2_1.jpg?ts=1606727889015',
+    'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_2_3_1.jpg?ts=1606727896369',
+    'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_2_4_1.jpg?ts=1606727898445',
+];
+
+const product = {
+    title: 'SOFT MINI CROSSBODY BAG WITH KISS LOCK',
+    description: [
+        'Mini crossbody bag available in various colours. Featuring two compartments. Handles and detachable crossbody shoulder strap. Lined interior. Clasp with two metal pieces.',
+        'Height x Length x Width: 14 x 21.5 x 4.5 cm. / 5.5 x 8.4 x 1.7"'
+    ],
+    price: '29.99£'
+}
+
+export default () => {
+    return <View>
+        <StatusBar hidden/>
+        <View style={{height: ITEM_HEIGHT, overflow: 'hidden'}}>
+          <FlatList 
+            data={images}
+            keyExtractor={(_, index) => index.toString()}
+            snapToInterval={ITEM_HEIGHT}
+            decelerationRate="fast"
+            renderItem={({item}) => {
+              return <View>
+                <Image source={{uri: item}} style={styles.image}/>
+              </View>
+            }}
+          />
+        </View>
     </View>
-  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  image: {
+    width: ITEM_WIDTH,
+    height: ITEM_HEIGHT,
+    resizeMode: 'cover'
+  }
+})
